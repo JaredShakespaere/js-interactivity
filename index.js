639bf7b24e3b0a19898c0f4ff1a1c63f092e4c20
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-09-29 17:23:51 -0500

    final-advanced-problems

--- index.js
+++ index.js
@@ -23,16 +23,24 @@ function addMovie(event) {
 const form= document.querySelector('form')
 form.addEventListener('submit', addMovie)
 
+
 function deleteMovie(event){
     event.target.parentNode.remove()
-    message.textContent= 'Movie Deleted!'
+    message.textContent= event.target.parentNode.childNodes[0].textContent +' Deleted!'
+    revealMessage()
 }
 
 function crossOffMovie(event){
     event.target.classList.toggle('checked')
     if(event.target.classList.contains('checked')){
-        message.textContent = 'Movie Watched!'
+        message.textContent = event.target.textContent + ' watched!'
     }else {
-        message.textContent = 'Movie added back!'
+        message.textContent = event.target.textContent +' added back!'
     }
+    revealMessage()
+}
+
+function revealMessage (){
+    message.classList.remove('hide')
+    setTimeout(() => {message.classList.add('hide')}, 1000);
 }
